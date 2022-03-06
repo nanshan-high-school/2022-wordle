dfc8656a755d625f244dff93484ce543fc80e7b8
Chaoray <65358638+Chaoray@users.noreply.github.com>
2022-03-06 15:36:12 +0800

    Update wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int indexOf(char, string);
+
 int main() {
     vector<string> words {
         "apple",
@@ -80,6 +82,60 @@ int main() {
 
     int wordCount = words.size();
     int questionIndex = rand() % wordCount;
+    string answer = words[questionIndex];
+    short guessCount = 0;
+  
+    printf("\x1b[46m\x1b[37m __      __                   __   ___             \n/\\ \\  __/\\ \\                 /\\ \\ /\\_ \\            \n\\ \\ \\/\\ \\ \\ \\    ___   _ __  \\_\\ \\\\//\\ \\      __   \n \\ \\ \\ \\ \\ \\ \\  / __`\\/\\`'__\\/'_` \\ \\ \\ \\   /'__`\\ \n  \\ \\ \\_/ \\_\\ \\/\\ \\L\\ \\ \\ \\//\\ \\L\\ \\ \\_\\ \\_/\\  __/ \n   \\ `\\___x___/\\ \\____/\\ \\_\\\\ \\___,_\\/\\____\\ \\____\\\n    '\\/__//__/  \\/___/  \\/_/ \\/__,_ /\\/____/\\/____/\n                                                   \n                                                   \n\x1b[0m");
+  
+    while(true) {
+        printf("\x1b[0m\n");
 
-    cout << "本次題目：" << words[questionIndex];
+        if(guessCount < 6) {
+          guessCount++;
+        } else {
+          printf("\n\x1b[1;31;42mNo more try\n\x1b[0m");
+          break;
+        }
+
+        string input;
+        printf("Answer:");
+        cin >> input;
+
+        if(answer.size() != input.size()) {
+          printf("Input length must be %d long\n", (int)answer.size());
+          continue;
+        }
+
+        if(input == "") {
+          printf("Say Something\n");
+          continue;
+        }
+
+        for(int i = 0; i < input.size(); i++) {
+          if(answer[i] == input[i]) {
+            printf("\x1b[32m%c", input[i]);  //green
+            } else {
+            if(indexOf(input[i], answer) != -1) {
+              printf("\x1b[33m%c", input[i]);  // yellow
+            } else {
+              printf("\x1b[37m%c", input[i]);  // white
+            }
+        }
+    }
+
+    if(answer == input) {
+      printf("\n\x1b[1;31;42mYou Win!!!\n\x1b[0m");  // red foreground with green background and reset the color to default
+      break;
+    }
+}
+
+int indexOf(char target, string src) {
+  int index = -1;
+  for(int i = 0; i < src.size(); i++) {
+    if(src[i] == target) {
+      index = i;
+    }
+  }
+
+  return index;
 }
